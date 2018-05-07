ff52cd1993399948ae12bbb7b0aa712966558337
skim <rumex13@naver.com>
2018-05-07 22:27:32 +0900

    초기 세팅

---
+++ swiftMemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swiftMemo
+//
+//  Created by KimSuyoung on 2018. 5. 7..
+//  Copyright © 2018년 KimSuyoung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
